d8648185ba5eee43e9f2ae037edad24383b39c24
Dylan Mace <dtmace2@gmail.com>
2021-06-23 13:09:56 -0400

    Initial Commit

---
+++ Boards/BoardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardsApp.swift
+//  Boards
+//
+//  Created by Dylan Mace on 6/23/21.
+//
+
+import SwiftUI
+
+@main
+struct BoardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
